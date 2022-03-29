a6d83eced6b4e61eb9669c5c2ba412fda2bcbc46
msnaye <99111936+msnaye@users.noreply.github.com>
2022-03-28 23:48:17 -0500

    working on js functions

---
+++ script.js
@@ -0,0 +1,14 @@
+function getHistory(){
+    return document.getElementById("historyValue").innerText;
+
+}
+
+function printHistory (num){
+        document.getElementById("historyValue").innerText=num;
+}
+function printOutput(){
+        document.getElementById("OutputValue").innerText;
+}
+function getOutput(num){
+        document.getElementById("OutputValue").innerText=num;
+}
